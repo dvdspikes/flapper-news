2145e85ba26877f829f678e0766263055dce4dc2
dvdspikes <dsikes@gmail.com>
2015-09-25 17:12:14 -0500

    Adding files

---
+++ models/Posts.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+PostSchema.methods.upvote = function(cb) {
+	console.log("Post.upvote");
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+mongoose.model('Post', PostSchema);
